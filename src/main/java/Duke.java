db0c492706109d9100e46dd69aac7434755c4c9c
e0425705 <e0425705@u.nus.edu>
2020-08-18 20:28:53 +0800

    Level-0. Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        
+        lines();
+        helloMessage();
+        lines();
+        byeMessage();
+        lines();
+    }
+
+    public static void lines() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void helloMessage() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void byeMessage() {
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
