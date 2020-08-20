430003429448e9b14a8cbcbcf901cf67c94873a1
e0425705 <e0425705@u.nus.edu>
2020-08-20 16:44:24 +0800

    Added Level-1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,41 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
+        // printing out of "DUKE"
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        
+
+        // Level 0
         lines();
         helloMessage();
         lines();
         byeMessage();
         lines();
+
+        // Level 1
+        lines();
+        helloMessage();
+        lines();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while(!(input.equals("bye"))) {
+            lines();
+            System.out.println(input);
+            lines();
+            Scanner next = new Scanner(System.in);
+            input = next.nextLine();
+        }
+
+            lines();
+            byeMessage();
+            lines();
+
     }
 
     public static void lines() {
