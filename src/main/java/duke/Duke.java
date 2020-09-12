7e2fd9243db7d2ccb780b2ffca6c71d042645c58
e0425705 <e0425705@u.nus.edu>
2020-09-12 20:55:47 +0800

    Revert "add command for delete in commandAvailable()"
    This reverts commit 4f86e20379ad7e10d10ed2ff362e69c9ec7c236a.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -127,7 +127,6 @@ public class Duke {
         System.out.println("todo x - x is the data to be done");
         System.out.println("deadline x /by y - x is the data and y is the deadline");
         System.out.println("event x /at y - x is the data and y is the event date");
-        System.out.println("delete x - removes the task at index x of the list ");
         System.out.println("bye - this terminates the program");
         drawLines();
     }
